9c9005d7c0ee0c3f28c506e8c1f349ba30a3d515
shovon <shuvon5358@gmail.com>
2025-01-06 00:12:11 +0600

    adding button action

--- Bank_Manager/src/bank_manager/FXMLDocumentController.java
+++ Bank_Manager/src/bank_manager/FXMLDocumentController.java
@@ -20,10 +20,7 @@ public class FXMLDocumentController implements Initializable {
     
     @FXML
     private Label label;
-    @FXML
-    private Button button;
     
-    @FXML
     private void handleButtonAction(ActionEvent event) {
         System.out.println("You clicked me!");
         label.setText("Hello World!");
