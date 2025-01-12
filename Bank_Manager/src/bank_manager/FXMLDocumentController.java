fe427e1e3efec9d001f1a9fd2b275bac68707252
shovon <shuvon5358@gmail.com>
2025-01-12 21:52:42 +0600

    2nd page adding

--- Bank_Manager/src/bank_manager/FXMLDocumentController.java
+++ Bank_Manager/src/bank_manager/FXMLDocumentController.java
@@ -4,13 +4,19 @@
  */
 package bank_manager;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
 
 /**
  *
@@ -34,7 +40,12 @@ public class FXMLDocumentController implements Initializable {
     }    
 
     @FXML
-    private void btnsaveaction1(ActionEvent event) {
+    private void btnsaveaction1(ActionEvent event) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("main.fxml"));
+        Parent root = loader.load();
+        Scene scene = new Scene(root);
+        Stage window = (Stage)(((Node)event.getSource()).getScene().getWindow());
+        window.setScene(scene);
     }
     
 }
